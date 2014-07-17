f029f60afc8033ac15f7e7e1bb3fb018d02ef0db
Patricia Castellanos <p.castellanos@vu.nl>
2014-07-17 16:52:04 -0400

    ../setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,8 @@ setup(
     author='P. Castellanos',
     author_email='patti.castellanos@gmail.com',
     packages = packages,
-    url='http://pypi.python.org/pypi/HDFTools/',
+    url='https://github.com/patticastellanos/hdftools',
     license='LICENSE.txt',
     description='Wrapper for pyhdf and PyTables',
     long_description=open('README.txt').read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
 )
