8d37cb5298757c254a46bf8546933aedf52eccae
Patricia Castellanos <p.castellanos@vu.nl>
2014-09-07 15:58:48 +0200

    function to list contents of file

---
+++ hdftools/listhdf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import pyhdf.HDF as HDF4
+import pyhdf.SD as SD
+import tables as HDF5
+#########################################################################
+def listhdf(hdf):
+    "a function to list the contents of the hdf file"
+    
+    if hdf.filetype is 'HDF4':
+        #look for SD data
+        if hdf.SD is not None:
+            print hdf.SD.datasets
